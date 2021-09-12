beceb6194b7e6c325c85e0f10ffab7f13d594c97
Cesar <cesar_silk321@hotmail.com>
2021-09-12 03:07:25 -0300

    base template pronto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {PrimeNGConfig} from "primeng/api";
 
+interface City {
+  name: string,
+  code: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +14,21 @@ import {PrimeNGConfig} from "primeng/api";
 export class AppComponent implements OnInit{
   title = 'electron-primeng';
 
+  cities: City[];
+
+  selectedCity: City | undefined;
 
   constructor(
     private primengConfig: PrimeNGConfig
   ) {
     this.primengConfig.ripple = true;
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+    ];
   }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,22 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {MultiSelectModule} from "primeng/multiselect";
+import {ListboxModule} from "primeng/listbox";
+import {ScrollPanelModule} from "primeng/scrollpanel";
+import {FormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
+import {SliderModule} from "primeng/slider";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {ToggleButtonModule} from "primeng/togglebutton";
+import {TreeSelectModule} from "primeng/treeselect";
+import {SplitButtonModule} from "primeng/splitbutton";
+import {SpeedDialModule} from "primeng/speeddial";
+import {PaginatorModule} from "primeng/paginator";
+import {PanelModule} from "primeng/panel";
 
 @NgModule({
   declarations: [
@@ -12,7 +28,23 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    MultiSelectModule,
+    ScrollPanelModule,
+    ListboxModule,
+    DropdownModule,
+    SliderModule,
+    SelectButtonModule,
+    ToggleButtonModule,
+    TreeSelectModule,
+    SplitButtonModule,
+    SpeedDialModule,
+    PaginatorModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
