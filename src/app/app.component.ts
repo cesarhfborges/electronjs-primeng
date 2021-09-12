aa72e3657bf1328554837df6607cca1c08bbd746
Cesar <cesar_silk321@hotmail.com>
2021-09-12 02:01:01 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'electron-primeng';
+
+
+  constructor(
+    private primengConfig: PrimeNGConfig
+  ) {
+    this.primengConfig.ripple = true;
+  }
+
+  ngOnInit(): void {
+  }
 }
